d194143482867d8eb85c10696fa051e2e9ee2f45
OTree <kevin10410@gmail.com>
2020-09-26 23:09:16 +0800

    refactor(app.js): refactor with routes/admin.js

--- app.js
+++ app.js
@@ -2,24 +2,13 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin.js');
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/add', (req, res, next) => {
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title">
-      <button type="submit">submit</button>
-    </form>
-  `)
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
+app.use(adminRoutes);
 
 app.use('/', (req, res, next) => {
   console.log('second middleware');
